e5462b38bd4a660b6596bcb1f4d55cea4ae9872b
wuw6248 <87937525+wuw6248@users.noreply.github.com>
2022-05-13 19:40:16 +0700

    Delete dist directory

--- dist/forms/MessageForm.d.ts
+++
@@ -1,9 +0,0 @@
-import type { MessageFormResponse } from '../types';
-import {Player} from 'mojang-minecraft'
-export declare class MessageFormData {
-    title(titleText: string): MessageFormData
-    body(bodyText: string): MessageFormData
-    button1(text: string): MessageFormData
-    button2(text: string): MessageFormData
-    show(player: Player): Promise<MessageFormResponse>
-}
